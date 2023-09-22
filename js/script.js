a9bd45c47697a827b9df526b42dc11cfaccfb9fe
Hemonta Mondal <86686198+hemontamondal@users.noreply.github.com>
2023-09-22 13:18:48 +0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -4,8 +4,21 @@
 
 $(document).ready(function(){
 
+	// old version
+	/*
+
 	$('button').click(function(){
 		alert("Welcome");
+	})
+
+	*/
+
+
+	// New version
+	$('button').on( 'click', function(){
+
+		alert("Hello World");
+
 	})
 
 })
